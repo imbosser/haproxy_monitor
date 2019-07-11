7e9f28fdb1b0532dacbbba3f93f2cbfc98b69f36
root <trung.cv@outlook.com>
2019-07-11 17:55:52 +0700

    dau phong

--- agent.py
+++ agent.py
@@ -1,1 +1,19 @@
-this is python file
+from haproxyadmin import haproxy
+hap = haproxy.HAProxy(socket_dir='/var/run/haproxy')
+frontends = hap.frontends()
+for frontend in frontends:
+ print(frontend.name, frontend.requests, frontend.process_nb)
+ FRONTEND_METRICS = ['scur', 'stot']
+ for m in FRONTEND_METRICS:
+    print("name {} value {}".format(m, frontend.metric(m)))
+
+backends = hap.backends()
+for backend in backends:
+   print(backend.name, backend.requests, backend.process_nb)
+   BACKEND_METRICS = ['scur', 'stot']
+   for m in BACKEND_METRICS:
+    print("name {} value {}".format(m, backend.metric(m)))
+   servers = backend.servers()
+   for server in servers:
+      print(" ", server.name, server.requests)
+
